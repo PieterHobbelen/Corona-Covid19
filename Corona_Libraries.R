0fee17a3d51f2041a98aa9b910b16890c88aff64
PieterHobbelen <pieter1224@gmail.com>
2020-06-21 13:47:15 +0200

    add libraries file, create version 1.0.1

---
+++ Corona_Libraries.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("ggrepel")
+install.packages("knitr")
+install.packages("kableExtra")
+
+library(tidyverse)
+library(ggrepel)
+library(knitr)
+library(kableExtra)
